2c6dedd10a86c3167b5d47f6e16ae2fc4f1bf6e6
乔不思 <3504852205@qq.com>
2023-11-22 14:45:58 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -3,50 +3,66 @@ import requests , json , random
 
 
 
-def select(a):
-    dict ={'Anime':'a','Comic':'b','Game':'c','Literature':'d','Original':'e','Internet':'f','Other':'g','Video':'h','Poem':'i','NCM':'j','Philosophy':'k','Funny':'l'}
-    if str(a) in dict.keys() :
-        return dict[str(a)]
-    else :
-        return dict[str(random.choice(list(dict.keys())))]
-
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-
-
-@app.route('/search/',methods=["GET"])
-def search():
-    query = request.args.get("s")
-    key = 'hitokoto'
-    url = requests.get("https://api.pwxiao.top/sentences/i.json")
-    text = url.text
-    data = json.loads(text)
-        
-    matches = [item for item in data if key in item and query in item[key]]
-    matches = json.dumps(matches,ensure_ascii=False)
-    return matches
-    
-
 
 @app.route('/',methods=["GET"])
-def return_OneText():
+def return_class():
+   headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 13; V2203A Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/1110005 MMWEBSDK/20230701 MMWEBID/7078 MicroMessenger/8.0.40.2420(0x28002858) WeChat/arm64 Weixin NetType/5G Language/zh_CN ABI/arm64',
+    'Referer': 'https://www.example.com',
+    'Cookie': 'PHPSESSID=ajrghtq0nm1ivf9baqjd64ihpp'
+    }
 
-    category = request.args.get("category")
 
-    category =  select(category)
-    url = requests.get("https://api.pwxiao.top/sentences/" + category + ".json")
-    text = url.text
-    # with open("../sentences/"+category+".json",'r',encoding='utf-8') as f:
-    #      text = f.read()       
+    # 发送GET请求
+    response = requests.get('http://alpro.xiaovei.cn:80/api/kebiao/getList', headers=headers)
 
-    res = json.loads(text)
+    # 检查响应状态码
+    if response.status_code == 200:
+        # 处理响应数据
+        data = response.text
 
-    try:
-        number = random.randint(0,(len(res) - 1))
-    except:
-        number = 6
 
-    result = res[number]
-    fina_res = json.dumps(result,ensure_ascii=False)
- 
+    res = json.loads(data)
+
+    course_list = res["data"]
+
+    tables = []
+    # 遍历课程列表
+    for course in course_list:
+        # 获取课程信息
+        
+        course_name = course["course_name"]
+        teacher = course["teacher"]
+        address = course["address"]
+        start_week = course["start_week"]
+        end_week = course["end_week"]
+        start_jie = course["start_jie"]
+        end_jie = course["end_jie"]
+        week = course["week"]
+        if(start_week<=13<=end_week and week==3):
+            mdict={
+                'course_name': course_name,
+                'teacher' : teacher,
+                'address' : address,
+                'start_jie' : start_jie,
+                'end_jie' : end_jie,
+                'week' : week,
+                'start_week' : start_week,
+                'end_week' : end_week
+            }
+            tables.append(mdict)
+
+    sorted_tables = sorted(tables,key=lambda d:d['start_jie'])
+    content = ''
+    for table in sorted_tables:
+        if(table['start_week']<=13<=table['end_week'] and table['week']==3):
+            # 打印课程信息
+            content+= table['course_name']+"\n"
+            content+= table['teacher']+'\n'
+            content+= table['address']+'\n'
+            content+= str(table['start_jie'])+" "+str(table['end_jie'])+"节"
+            content+= '\n\n'
+
+    fina_res = json.dumps(content,ensure_ascii=False)
+    
     return fina_res
